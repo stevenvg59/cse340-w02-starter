5f8ef00e544ed14ee27c88d12e00c6ffcbc8e563
Steven <stevensvg59@gmail.com>
2023-10-13 12:56:06 -0500

    Adding route to the server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -24,10 +25,9 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 // Index route
 app.get("/", baseController.buildHome)
-// app.get("/", function(req, res){
-//   res.render("index", {title: "Home"})
-// })
-app.use(static)
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 /* ***********************
  * Local Server Information

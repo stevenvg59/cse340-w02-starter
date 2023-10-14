48d7d1c32efac8e905643a8591dae33dded7f335
Steven <stevensvg59@gmail.com>
2023-10-14 11:36:30 -0500

    Minor changes for classification view in HTML

--- utilities/index.js
+++ utilities/index.js
@@ -35,8 +35,8 @@ Util.buildClassificationGrid = async function(data){
       grid += '<li>'
       grid += '<a href="../../inv/detail/' + vehicle.inv_id
       + '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model
-      + 'details"><img src="' + vehicle.inv_thumbnail
-      + '"alt=Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model
+      + ' details"><img src="' + vehicle.inv_thumbnail
+      + '"alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model
       + ' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'

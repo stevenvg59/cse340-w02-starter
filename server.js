45ae7f23fb1de5dede27779ebc15cb9503d24186
Steven <stevensvg59@gmail.com>
2023-10-21 23:50:03 -0500

    Error handling implemmented

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +30,29 @@ app.get("/", baseController.buildHome)
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
+/* ***********************
+ * File Not Found Route - must be last route in list
+ * Place after all routes
+ * Unit 3, Basic Error Handling Activity
+ *************************/
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* *************************
+ * Express Error Handler
+ * Place after all other middleware
+ * Unit 3, Basic Error Handling Activity
+ * *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
@@ -36,6 +60,8 @@ app.use("/inv", inventoryRoute)
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+
+
 
 /* ***********************
  * Log statement to confirm server operation
